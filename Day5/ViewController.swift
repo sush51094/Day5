3f9bb19943ea459ce7e77ad1e476e869c3afddf3
MacStudent <macstudent@126-iMac12.local>
2019-03-08 19:12:25 -0500

    changes added

--- Day5/ViewController.swift
+++ Day5/ViewController.swift
@@ -8,13 +8,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
+    
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var picker: UIPickerView!
+    
+    let countrylist = ["India","Australia","America","canada","qatar","france","germany","Newzeland","Japan","china"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.picker.delegate = self
+        self.picker.dataSource = self
+        
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func birthDatePicker(_ sender: UIDatePicker) {
+        
+        
+        print(sender.date)
+        
+       // print(sender.locale?.currencyCode)
+        
+        
+        
+        
+    }
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+      return self.countrylist.count
+        
+        
+    }
+    
+    
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+       // return "Comp: \(component)-Row :\(row)"
+        
+        return self.countrylist[row]
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+    }
+    
+    
 }
 
